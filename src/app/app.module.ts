9cfad1c9cfe67fae0fe0a72b2aae9324f9237aa0
meTheLearner <togunchan@gmail.com>
2020-12-09 02:31:31 +0300

    1-Projenin Hazırlanması

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ProductsComponent } from './products/products.component';
+import { UsersComponent } from './users/users.component';
+import { CategoriesComponent } from './categories/categories.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductsComponent,
+    UsersComponent,
+    CategoriesComponent
   ],
   imports: [
     BrowserModule
